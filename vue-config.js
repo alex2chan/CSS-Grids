6908e33e1d8b58d37ae08c8ebf1981a6ab159b7a
Alex Chandra <alex2chandra@hotmail.com>
2019-06-03 17:46:46 +0800

    amend

--- vue-config.js
+++ vue-config.js
@@ -1,11 +1,6 @@
 module.exports = {
   baseUrl: "././",
   publicPath: process.env.NODE_ENV === 'production'
-<<<<<<< HEAD
     ? '/CSS-Grids/'
     : '/'
-=======
-    ? './CSS-Grids/'
-    : './'
->>>>>>> b6c46cf5c6ceb34031355226cf90c9d948b877b4
 }
